08fee1c41fd3867a31657d494ca082350fee243c
Rossi Kwak <rossi.kwak@gmail.com>
2022-06-08 17:30:26 +0900

    CListCtrl 컨트롤 추가

--- gLinkBudget/resource.h
+++ gLinkBudget/resource.h
@@ -7,8 +7,8 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_GLINKBUDGET_DIALOG          102
 #define IDR_MAINFRAME                   128
-#define IDC_LIST1                       1000
 #define IDC_LOG_LIST                    1000
+#define IDC_FREE_SPACE_PATH_LOSS_LIST   1001
 
 // Next default values for new objects
 // 
@@ -16,7 +16,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
