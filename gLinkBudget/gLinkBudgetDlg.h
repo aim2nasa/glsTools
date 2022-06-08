13a7773510de5e175c81de4fa8dec9c062abf028
Rossi Kwak <rossi.kwak@gmail.com>
2022-06-08 15:00:15 +0900

    glsTools 솔루션 gLinkBudget 프로젝트 최초 생성

---
+++ gLinkBudget/gLinkBudgetDlg.h
@@ -0,0 +1,34 @@
+﻿
+// gLinkBudgetDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CgLinkBudgetDlg 대화 상자
+class CgLinkBudgetDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CgLinkBudgetDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_GLINKBUDGET_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
