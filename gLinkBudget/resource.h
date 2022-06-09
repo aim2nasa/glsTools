bddf49a99002f8be245df76088d7607a349b1e82
Rossi Kwak <rossi.kwak@gmail.com>
2022-06-09 16:22:35 +0900

    GLS 로고 표시

--- gLinkBudget/resource.h
+++ gLinkBudget/resource.h
@@ -7,6 +7,7 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_GLINKBUDGET_DIALOG          102
 #define IDR_MAINFRAME                   128
+#define IDB_GLS_BITMAP                  130
 #define IDC_LOG_LIST                    1000
 #define IDC_FREE_SPACE_PATH_LOSS_LIST   1001
 #define IDC_RECEIVER_SIGNAL_LEVEL_LIST  1002
@@ -16,7 +17,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
