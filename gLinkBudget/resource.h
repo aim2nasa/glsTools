c71884ede6fcfa4167de9a38d81f1c8b17c529e4
Rossi Kwak <rossi.kwak@gmail.com>
2022-06-09 16:39:07 +0900

    아이콘 변경

--- gLinkBudget/resource.h
+++ gLinkBudget/resource.h
@@ -2,11 +2,11 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // gLinkBudget.rc에서 사용되고 있습니다.
 //
+#define IDR_MAINFRAME                   5
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
 #define IDD_GLINKBUDGET_DIALOG          102
-#define IDR_MAINFRAME                   128
 #define IDB_GLS_BITMAP                  130
 #define IDC_LOG_LIST                    1000
 #define IDC_FREE_SPACE_PATH_LOSS_LIST   1001
@@ -17,7 +17,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
